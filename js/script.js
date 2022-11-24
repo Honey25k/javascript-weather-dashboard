3b433a2b62a1c4bb15614eec712261f42c90b884
honeytech <honey93k@gmail.com>
2022-11-23 23:07:31 -0800

    added comments to script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// global variables to bring important info 
 var apiKey = '9fbbc9e521bd2169ee98d90be530aa58';
 var searchButton = document.querySelector('#search-button');
 var cityInput = document.querySelector('#city-input');
@@ -6,10 +7,9 @@ var latestTemperature = document.querySelector('#current-temp');
 var windSpeed = document.querySelector('#wind-speed');
 var humidity = document.querySelector('#current-humidity');
 
-
 var fiveDayForecast = document.querySelectorAll('#forecast-date');
 
-
+// function that uses api call for city search, give us the latitude and longitude.    
 var getLatLon = function (event) {
     var searchEl = document.querySelector('#weather-search');
     var userInput = searchEl.value;
@@ -27,12 +27,13 @@ var getLatLon = function (event) {
     })   
 }
 
-
+// function to get current days weather by using api url.  
 var getcurrentForecast = function (lat, lon) {
     var forecastLink = 'https://api.openweathermap.org/data/2.5/weather'; 
     forecastLink = forecastLink + '?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey + '&units=imperial';
      
-
+    /*fetch for getting the information from api url 
+    then we use that information to give us a date, temp, wind speed and humidity */
     fetch(forecastLink)
     .then(function (response) {
         return response.json();
@@ -53,7 +54,7 @@ var getcurrentForecast = function (lat, lon) {
 }
 
 
-
+// this function will give us the next five days weather using the forecast api url  
 var getForecast = function (lat, lon) {
     var forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast'; 
     forecastUrl = forecastUrl + '?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey + '&units=imperial';
@@ -72,6 +73,7 @@ var getForecast = function (lat, lon) {
             fiveDayForecast[i-1].innerHTML = futureFiveDays;
         }
         
+        // here we are adding icons for weather conditions
         var forecastIcon = response.list[4].weather[0].icon;
         var weatherIconUrl = 'https://openweathermap.org/img/wn/' + forecastIcon + '.png';
         document.querySelector('#weather-icon-1').src = weatherIconUrl;
@@ -93,14 +95,14 @@ var getForecast = function (lat, lon) {
         document.querySelector('#weather-icon-5').src = weatherIconUrl;
 
         
-        
+        // adding the weather forecast for the five days ahead.  
         var forecastTemp = document.querySelectorAll('#forecast-temp-1');
         forecastTemp[0].innerHTML = 'Temp: ' + response.list[4].main.temp + ' °F';
         var forecastWind = document.querySelectorAll('#forecast-wind-1');
         forecastWind[0].innerHTML = 'Wind: ' + response.list[4].wind.speed + ' MPH';
         var forecastHumidity = document.querySelectorAll('#forecast-humidity-1');
         forecastHumidity[0].innerHTML = 'Humidity: ' + response.list[4].main.humidity + ' %';
-
+        
         forecastTemp = document.querySelectorAll('#forecast-temp-2');
         forecastTemp[0].innerHTML = 'Temp: ' + response.list[12].main.temp + ' °F';
         forecastWind = document.querySelectorAll('#forecast-wind-2');
@@ -130,7 +132,7 @@ var getForecast = function (lat, lon) {
         forecastHumidity[0].innerHTML = 'Humidity: ' + response.list[36].main.humidity + ' %';
     })
 }
-
+// save searches to a list below our input for cities. 
 var searchListContainer = document.querySelector('#history-list');
 
 function listOfHistory () {
@@ -143,12 +145,12 @@ function listOfHistory () {
     var searchHistoryItem = JSON.parse(localStorage.getItem('city'));
     console.log(searchHistoryItem);
 } 
-
+// click function for search button 
 searchButton.addEventListener('click', function () {
     getLatLon();
     listOfHistory();
 })
-
+// adding click function to history of cities so we can click on previous searched cities
 searchListContainer.addEventListener('click', function (event) {
     console.log(event.target.textContent);
     var searchEl = document.querySelector('#weather-search');
